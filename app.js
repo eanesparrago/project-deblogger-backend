e2290b1de7a7557c8d7a52311ad3634ba080e7e7
iansanwich <iansanwich@gmail.com>
2020-08-07 14:54:24 +0800

    Setup app.js

---
+++ app.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const isProduction = process.env.NODE_ENV === "production";
+
+// DB
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connected");
+  });
+
+// Middlewares
+app.use(morgan("dev"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "conduit",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Cors
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: process.env.CLIENT_URL }));
+}
+
+/// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+/// Error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function (err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+// Port
+const port = process.env.PORT || 8000;
+
+const server = app.listen(port, function () {
+  console.log("Listening on port " + server.address().port);
+});
