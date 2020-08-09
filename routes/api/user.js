e8cab48584a917618a00dced0c1d7e39f3432ff0
iansanwich <iansanwich@gmail.com>
2020-08-09 18:13:15 +0800

    Feat: Add user validators

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,20 @@
 const router = require("express").Router();
-const { create, logIn, read, update, getPhotoByUsername, getPublicProfile } = require("../../controllers/user");
+const {
+  create,
+  logIn,
+  read,
+  update,
+  getPhotoByUsername,
+  getPublicProfile,
+} = require("../../controllers/user");
 const auth = require("../auth");
+const {
+  validateForgotPassword,
+  validateResetPassword,
+  validateUserSignin,
+  validateUserSignup,
+} = require("../../validators/user");
+const { runValidation } = require("../../validators/index");
 
 router.get("/test", (req, res) => {
   res.json({
@@ -9,21 +23,21 @@ router.get("/test", (req, res) => {
 });
 
 // Create user
-router.post("/user", create);
+router.post("/user", validateUserSignup, runValidation, create);
 
 // Login user
-router.post("/user/login", logIn);
+router.post("/user/login", validateUserSignin, runValidation, logIn);
 
 // Read own user
 router.get("/user/profile", auth.required, read);
 
 // Update user
-router.put("/user", auth.required, update)
+router.put("/user", auth.required, update);
 
 // Get photo by username
-router.get("/user/photo/:username", getPhotoByUsername)
+router.get("/user/photo/:username", getPhotoByUsername);
 
 // Read public profile
-router.get("/user/:username", getPublicProfile)
+router.get("/user/:username", getPublicProfile);
 
 module.exports = router;
--- validators/user.js
+++ validators/user.js
@@ -1,5 +1,32 @@
 const { check } = require("express-validator");
 
-exports.userCreateValidator = [
-  
-]
+exports.validateUserSignup = [
+  check("name").not().isEmpty().withMessage("Name is required"),
+  check("email").isEmail().withMessage("Must be a valid email address"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];
+
+exports.validateUserSignin = [
+  check("email").isEmail().withMessage("Must be a valid email address"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];
+
+exports.validateForgotPassword = [
+  check("email")
+    .not()
+    .isEmpty()
+    .isEmail()
+    .withMessage("Must be a valid email address"),
+];
+
+exports.validateResetPassword = [
+  check("newPassword")
+    .not()
+    .isEmpty()
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];
